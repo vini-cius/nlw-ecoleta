7318f4adb3cdda6b71a6f2cde4ae18323a8c85a4
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2020-06-03 23:35:00 -0300

    backend finalizado

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -3,6 +3,24 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+	async index(req: Request, res: Response) {
+		const { city, uf, items } = req.query;
+
+		const parsedItems = String(items)
+			.split(',')
+			.map((item) => Number(item.trim()));
+
+		const points = await knex('points')
+			.join('point_items', 'points.id', '=', 'point_items.point_id')
+			.whereIn('point_items.item_id', parsedItems)
+			.where('city', String(city))
+			.where('uf', String(uf))
+			.distinct()
+			.select('points.*');
+
+		return res.json(points);
+	}
+
 	async show(req: Request, res: Response) {
 		const { id } = req.params;
 
@@ -35,7 +53,8 @@ class PointsController {
 		const trx = await knex.transaction();
 
 		const point = {
-			image: 'image-fake',
+			image:
+				'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
 			name,
 			email,
 			whatsapp,
@@ -57,6 +76,8 @@ class PointsController {
 		});
 
 		await trx('point_items').insert(pointItems);
+
+		await trx.commit();
 
 		return res.json({
 			id: point_id,
