636737a68c21cb1c65fe3294b487399711853482
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2020-06-04 21:47:35 -0300

    buscar itens de coleta da api

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
 
+interface Item {
+	id: number;
+	title: string;
+	image_url: string;
+}
+
 const CreatePoint: React.FC = () => {
+	const [items, setItems] = useState<Item[]>([]);
+
+	useEffect(() => {
+		api.get('/items').then((res) => {
+			setItems(res.data);
+		});
+	}, []);
+
 	return (
 		<div id="page-create-point">
 			<header>
@@ -85,30 +100,12 @@ const CreatePoint: React.FC = () => {
 					</legend>
 
 					<ul className="items-grid">
-						<li>
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-							<span>Óleo de cozinha</span>
-						</li>
-						<li className="selected">
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-							<span>Óleo de cozinha</span>
-						</li>
-						<li>
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-							<span>Óleo de cozinha</span>
-						</li>
-						<li>
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-							<span>Óleo de cozinha</span>
-						</li>
-						<li>
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-							<span>Óleo de cozinha</span>
-						</li>
-						<li>
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-							<span>Óleo de cozinha</span>
-						</li>
+						{items.map((item) => (
+							<li key={item.id}>
+								<img src={item.image_url} alt={item.title} />
+								<span>{item.title}</span>
+							</li>
+						))}
 					</ul>
 				</fieldset>
 
