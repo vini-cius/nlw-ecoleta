9879456826feaffe8d53776dbe1f274faca338ef
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2020-06-04 22:29:38 -0300

    buscar estados e cidades da api do ibge

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import axios from 'axios';
 import api from '../../services/api';
 
 import './styles.css';
@@ -13,14 +14,63 @@ interface Item {
 	image_url: string;
 }
 
+interface IBGEUFResponse {
+	sigla: string;
+}
+
+interface IBGECityResponse {
+	nome: string;
+}
+
 const CreatePoint: React.FC = () => {
 	const [items, setItems] = useState<Item[]>([]);
+	const [ufs, setUfs] = useState<string[]>([]);
+	const [selectedUf, setSelectedUf] = useState('0');
+
+	const [cities, setCities] = useState<string[]>([]);
+	const [selectedCity, setSelectedCity] = useState('0');
 
 	useEffect(() => {
 		api.get('/items').then((res) => {
 			setItems(res.data);
 		});
 	}, []);
+
+	useEffect(() => {
+		axios
+			.get<IBGEUFResponse[]>(
+				'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+			)
+			.then((res) => {
+				const ufInitials = res.data.map((uf) => uf.sigla);
+				setUfs(ufInitials);
+			});
+	}, []);
+
+	useEffect(() => {
+		if (selectedUf === '0') {
+			return;
+		}
+
+		axios
+			.get<IBGECityResponse[]>(
+				`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+			)
+			.then((res) => {
+				const cityNames = res.data.map((city) => city.nome);
+				setCities(cityNames);
+			});
+	}, [selectedUf]);
+
+	function handleSelectUf(e: ChangeEvent<HTMLSelectElement>) {
+		const uf = e.target.value;
+		setSelectedUf(uf);
+	}
+
+	function handleSelectCity(e: ChangeEvent<HTMLSelectElement>) {
+		const city = e.target.value;
+		setSelectedCity(city);
+	}
 
 	return (
 		<div id="page-create-point">
@@ -79,15 +129,35 @@ const CreatePoint: React.FC = () => {
 					<div className="field-group">
 						<div className="field">
 							<label htmlFor="uf">Estado (UF)</label>
-							<select name="uf" id="uf">
+							<select
+								name="uf"
+								id="uf"
+								value={selectedUf}
+								onChange={handleSelectUf}
+							>
 								<option value="0">Selecione uma UF</option>
+								{ufs.map((uf) => (
+									<option key={uf} value={uf}>
+										{uf}
+									</option>
+								))}
 							</select>
 						</div>
 
 						<div className="field">
 							<label htmlFor="city">Cidade</label>
-							<select name="city" id="city">
+							<select
+								name="city"
+								id="city"
+								value={selectedCity}
+								onChange={handleSelectCity}
+							>
 								<option value="0">Selecione uma cidade</option>
+								{cities.map((city) => (
+									<option key={city} value={city}>
+										{city}
+									</option>
+								))}
 							</select>
 						</div>
 					</div>
