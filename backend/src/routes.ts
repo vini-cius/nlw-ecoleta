4a3861d596e80759cb743a097276870434f24dbb
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2020-06-06 23:39:02 -0300

    feat: validação de dados no backend

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
+
 import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
@@ -13,7 +15,28 @@ const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+	'/points',
+	upload.single('image'),
+	celebrate(
+		{
+			body: Joi.object().keys({
+				name: Joi.string().required(),
+				email: Joi.string().required(),
+				whatsapp: Joi.number().required(),
+				latitude: Joi.number().required(),
+				longitude: Joi.number().required(),
+				city: Joi.string().required(),
+				uf: Joi.string().required().max(2),
+				items: Joi.string().required(),
+			}),
+		},
+		{
+			abortEarly: false,
+		}
+	),
+	pointsController.create
+);
 routes.get('/points/', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
