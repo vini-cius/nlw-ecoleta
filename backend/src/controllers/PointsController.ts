f172fc787d75d61bad0fc9ff7573f1edcad00b48
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2020-06-06 23:20:59 -0300

    feat: upload de imagens

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -18,7 +18,14 @@ class PointsController {
 			.distinct()
 			.select('points.*');
 
-		return res.json(points);
+		const serializedPoints = points.map((point) => {
+			return {
+				...point,
+				image_url: `http://192.168.15.13:3333/uploads/${point.image}`,
+			};
+		});
+
+		return res.json(serializedPoints);
 	}
 
 	async show(req: Request, res: Response) {
@@ -35,7 +42,12 @@ class PointsController {
 			.where('point_items.point_id', id)
 			.select('items.title');
 
-		return res.json({ point, items });
+		const serializedPoint = {
+			...point,
+			image_url: `http://192.168.15.13:3333/uploads/${point.image}`,
+		};
+
+		return res.json({ serializedPoint, items });
 	}
 
 	async create(req: Request, res: Response) {
@@ -53,8 +65,7 @@ class PointsController {
 		const trx = await knex.transaction();
 
 		const point = {
-			image:
-				'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+			image: req.file.filename,
 			name,
 			email,
 			whatsapp,
@@ -68,12 +79,15 @@ class PointsController {
 
 		const point_id = insertedIds[0];
 
-		const pointItems = items.map((item_id: number) => {
-			return {
-				item_id,
-				point_id,
-			};
-		});
+		const pointItems = items
+			.split(',')
+			.map((item: string) => Number(item.trim()))
+			.map((item_id: number) => {
+				return {
+					item_id,
+					point_id,
+				};
+			});
 
 		await trx('point_items').insert(pointItems);
 
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -4,7 +4,9 @@ import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet';
+
 import api from '../../services/api';
+import Dropzone from '../../components/Dropzone';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -48,6 +50,8 @@ const CreatePoint: React.FC = () => {
 	});
 
 	const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+	const [selectedFile, setSelectedFile] = useState<File>();
 
 	const history = useHistory();
 
@@ -132,16 +136,20 @@ const CreatePoint: React.FC = () => {
 		const [latitude, longitude] = selectedPosition;
 		const items = selectedItems;
 
-		const data = {
-			name,
-			email,
-			whatsapp,
-			uf,
-			city,
-			latitude,
-			longitude,
-			items,
-		};
+		const data = new FormData();
+
+		data.append('name', name);
+		data.append('email', email);
+		data.append('whatsapp', whatsapp);
+		data.append('uf', uf);
+		data.append('city', city);
+		data.append('latitude', String(latitude));
+		data.append('longitude', String(longitude));
+		data.append('items', items.join(','));
+
+		if (selectedFile) {
+			data.append('image', selectedFile);
+		}
 
 		try {
 			await api.post('/points', data);
@@ -170,6 +178,8 @@ const CreatePoint: React.FC = () => {
 					Cadastro do <br />
 					ponto de coleta
 				</h1>
+
+				<Dropzone onFileUploaded={setSelectedFile} />
 
 				<fieldset>
 					<legend>
