985ed0bec144eed6689fc2eb0a5cb65993eae17a
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2020-06-10 23:07:40 -0300

    conversão para styled-components finalizada

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from "react";
 import {
-  View,
-  StyleSheet,
   TouchableOpacity,
-  Text,
   ScrollView,
   SafeAreaView,
-  Image,
   Alert,
 } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { Feather as Icon } from "@expo/vector-icons";
-import MapView, { Marker } from "react-native-maps";
 import { SvgUri } from "react-native-svg";
 import * as Location from "expo-location";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Title,
+  Description,
+  MapContainer,
+  Map,
+  MapMarker,
+  MapMarkerContainer,
+  MapMarkerImage,
+  MapMarkerTitle,
+  ItemsContainer,
+  ItemTitle,
+  Item,
+} from "./styles";
 
 import api from "../../services/api";
 
@@ -122,15 +130,12 @@ const Points: React.FC = () => {
           <Icon name="arrow-left" size={20} color="#34cb79" />
         </TouchableOpacity>
 
-        <Text style={styles.title}>Bem vindo.</Text>
-        <Text style={styles.description}>
-          Econtre no mapa um ponto de coleta.
-        </Text>
+        <Title>Bem vindo.</Title>
+        <Description>Econtre no mapa um ponto de coleta.</Description>
 
-        <View style={styles.mapContainer}>
+        <MapContainer>
           {initialPosition[0] !== 0 && (
-            <MapView
-              style={styles.map}
+            <Map
               initialRegion={{
                 latitude: initialPosition[0],
                 longitude: initialPosition[1],
@@ -139,147 +144,51 @@ const Points: React.FC = () => {
               }}
             >
               {points.map((point) => (
-                <Marker
+                <MapMarker
                   key={String(point.id)}
-                  style={styles.mapMarker}
                   onPress={() => handleNavigateToDetail(point.id)}
                   coordinate={{
                     latitude: point.latitude,
                     longitude: point.longitude,
                   }}
                 >
-                  <View style={styles.mapMarkerContainer}>
-                    <Image
-                      style={styles.mapMarkerImage}
+                  <MapMarkerContainer>
+                    <MapMarkerImage
                       source={{
                         uri: point.image,
                       }}
                     />
-                    <Text style={styles.mapMarkerTitle}>{point.name}</Text>
-                  </View>
-                </Marker>
+                    <MapMarkerTitle>{point.name}</MapMarkerTitle>
+                  </MapMarkerContainer>
+                </MapMarker>
               ))}
-            </MapView>
+            </Map>
           )}
-        </View>
+        </MapContainer>
       </Container>
 
-      <View style={styles.itemsContainer}>
+      <ItemsContainer>
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={{ paddingHorizontal: 20 }}
         >
           {items.map((item) => (
-            <TouchableOpacity
-              style={[
-                styles.item,
-                selectedItems.includes(item.id) ? styles.selectedItem : {},
-              ]}
+            <Item
+              selectedItems={selectedItems}
+              itemId={item.id}
               onPress={() => handleSelectItem(item.id)}
               key={String(item.id)}
               activeOpacity={0.6}
             >
               <SvgUri width={42} height={42} uri={item.image_url} />
-              <Text style={styles.itemTitle}>{item.title}</Text>
-            </TouchableOpacity>
+              <ItemTitle>{item.title}</ItemTitle>
+            </Item>
           ))}
         </ScrollView>
-      </View>
+      </ItemsContainer>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {},
-
-  title: {
-    fontSize: 20,
-    fontFamily: "Ubuntu_700Bold",
-    marginTop: 24,
-  },
-
-  description: {
-    color: "#6C6C80",
-    fontSize: 16,
-    marginTop: 4,
-    fontFamily: "Roboto_400Regular",
-  },
-
-  mapContainer: {
-    flex: 1,
-    width: "100%",
-    borderRadius: 10,
-    overflow: "hidden",
-    marginTop: 16,
-  },
-
-  map: {
-    width: "100%",
-    height: "100%",
-  },
-
-  mapMarker: {
-    width: 90,
-    height: 80,
-  },
-
-  mapMarkerContainer: {
-    width: 90,
-    height: 70,
-    backgroundColor: "#34CB79",
-    flexDirection: "column",
-    borderRadius: 8,
-    overflow: "hidden",
-    alignItems: "center",
-  },
-
-  mapMarkerImage: {
-    width: 90,
-    height: 45,
-    resizeMode: "cover",
-  },
-
-  mapMarkerTitle: {
-    flex: 1,
-    fontFamily: "Roboto_400Regular",
-    color: "#FFF",
-    fontSize: 13,
-    lineHeight: 23,
-  },
-
-  itemsContainer: {
-    flexDirection: "row",
-    marginTop: 16,
-    marginBottom: 32,
-  },
-
-  item: {
-    backgroundColor: "#fff",
-    borderWidth: 2,
-    borderColor: "#eee",
-    height: 120,
-    width: 120,
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    paddingTop: 20,
-    paddingBottom: 16,
-    marginRight: 8,
-    alignItems: "center",
-    justifyContent: "space-between",
-    textAlign: "center",
-  },
-
-  selectedItem: {
-    borderColor: "#34CB79",
-    borderWidth: 2,
-  },
-
-  itemTitle: {
-    fontFamily: "Roboto_400Regular",
-    textAlign: "center",
-    fontSize: 13,
-  },
-});
 
 export default Points;
